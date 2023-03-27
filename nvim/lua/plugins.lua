f7eb6312e997df27df21500377b552a61f7875df
Dai Yamashita <dai.yamashita@trioptima.com>
2023-03-27 16:04:06 +0200

    Fix nvim plugin duplication

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -24,11 +24,6 @@ return require("packer").startup(function(use)
 
 	use {
 		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate"
-	}
-
-	use {
-		"nvim-treesitter/nvim-treesitter",
 		run = function() local ts_update = require("nvim-treesitter.install").update({ with_sync = true }) ts_update() end,
 	}
 
