af9203a8417fc367c2a035b77b549571fdc41d6b
dymst <dai.org@gmail.com>
2023-03-27 21:18:21 +0200

    Add .vimrc and update nvim plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -27,6 +27,8 @@ return require("packer").startup(function(use)
 		run = function() local ts_update = require("nvim-treesitter.install").update({ with_sync = true }) ts_update() end,
 	}
 
+	use "kdheepak/lazygit.nvim"
+
 	use "hrsh7th/nvim-cmp"
 	use "hrsh7th/cmp-nvim-lsp"
 	use "hrsh7th/cmp-nvim-lua"
