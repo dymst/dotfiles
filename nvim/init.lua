9cff1bf20173078334b8234362c0352bc324a97d
dymst <dai.org@gmail.com>
2023-03-29 21:44:48 +0200

    Adjust nvim plugin settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,9 @@
 require("plugins")
-require("neovim-tree")
-require("mason-config")
+
 require("custom-keymaps")
+require("mason-config")
+require("neovim-tree")
 
 vim.opt.clipboard = "unnamedplus"
+vim.cmd("colorscheme industry")
 vim.cmd("source ~/.vimrc")
