ad8a175c0857ade56a8ceb91b5f128ca137df4d8
dymst <dai.org@gmail.com>
2023-04-01 19:15:43 +0200

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@ require("plugins")
 require("custom-keymaps")
 require("mason-config")
 require("neovim-tree")
+require("treesitter-config")
 
 vim.opt.clipboard = "unnamedplus"
 vim.cmd("colorscheme industry")
