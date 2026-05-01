5a004c8b18bad28905f4c7ea7bd7e9a4bcea2b7d
dymst <dai.org@gmail.com>
2026-05-01 18:58:12 +0200

    nvim: add support for debugging C++ with arguments and launch.json

--- nvim/lua/dap-config.lua
+++ nvim/lua/dap-config.lua
@@ -29,9 +29,29 @@ require("nvim-dap-virtual-text").setup()
 -- Go setup
 require("dap-go").setup()
 
+-- Load VS Code launch.json if it exists
+require('dap.ext.vscode').load_launchjs(nil, { cppdbg = {'c', 'cpp'}, codelldb = {'c', 'cpp'} })
+
 dap.listeners.after.event_initialized["dapui_config"] = dapui.open
 dap.listeners.before.event_terminated["dapui_config"] = dapui.close
 dap.listeners.before.event_exited["dapui_config"] = dapui.close
 
--- Custom C++ adapter configuration (if needed, though mason-nvim-dap handles most)
--- codelldb is usually sufficient for C++ debugging on Linux.
+-- Custom C++ configuration to prompt for arguments
+dap.configurations.cpp = {
+  {
+    name = "Launch with Arguments",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    args = function()
+      local args_string = vim.fn.input('Arguments: ')
+      return vim.split(args_string, " +")
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+  },
+}
+
+dap.configurations.c = dap.configurations.cpp
