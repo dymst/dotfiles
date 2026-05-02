c3e5c766ef0186d424d08270d72a0be684fa1128
dymst <dai.org@gmail.com>
2026-05-02 11:55:55 +0200

    nvim: add mnemonic debug mappings to avoid F10/F11 terminal conflicts

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -19,6 +19,12 @@ vim.keymap.set('n', '<S-F5>', function() require('dap').terminate() end, { desc 
 vim.keymap.set('n', '<F10>', function() require('dap').step_over() end, { desc = 'Debug: Step Over' })
 vim.keymap.set('n', '<F11>', function() require('dap').step_into() end, { desc = 'Debug: Step Into' })
 vim.keymap.set('n', '<S-F11>', function() require('dap').step_out() end, { desc = 'Debug: Step Out' })
+
+-- Mnemonic alternatives (in case F-keys are captured by terminal/OS)
+vim.keymap.set('n', '<leader>c', function() require('dap').continue() end, { desc = 'Debug: Continue' })
+vim.keymap.set('n', '<leader>n', function() require('dap').step_over() end, { desc = 'Debug: Step Over (Next)' })
+vim.keymap.set('n', '<leader>si', function() require('dap').step_into() end, { desc = 'Debug: Step Into' })
+vim.keymap.set('n', '<leader>so', function() require('dap').step_out() end, { desc = 'Debug: Step Out' })
 vim.keymap.set('n', '<leader>b', function() require('dap').toggle_breakpoint() end, { desc = 'Debug: Toggle Breakpoint' })
 vim.keymap.set('n', '<leader>B', function()
   require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))
